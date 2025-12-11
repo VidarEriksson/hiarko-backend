71795f37de0f43aafd7b6f52a86f1c6d074cfdcf
VidarEriksson <98378681+VidarEriksson@users.noreply.github.com>
2025-12-11 20:59:28 +0100

    add start for auth (#6)

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
 dotenv.config();
+import authRoutes from './routes/auth';
 
 const app = express();
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('hello world');
 });
+
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
