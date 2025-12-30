3f611ce03b7e4eba90606a2d5952d092baefd977
vidar <vidar03@hotmail.com>
2025-12-30 13:05:12 +0100

    restruct auth routes and logic

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,72 +1,11 @@
 import { Router } from "express";
-import jwt from "jsonwebtoken";
 import { authenticateToken } from "../middleware/authMiddleware";
-import bcrypt from "bcrypt";
-import prisma from "../prisma/client";
-import { AuthenticatedRequest } from "../types/authenticated";
+import * as authController from "../controllers/auth.controller";
 
 const router = Router();
 
-const JWT_SECRET = process.env.JWT_SECRET;
-const JWT_EXPIRES_IN = "1h";
-if (!JWT_SECRET) {
-    throw new Error("JWT_SECRET is not defined in environment variables");
-};
-
-
-router.post("/login", async (req, res) => {
-  if(!req.body|| !req.body.email || !req.body.password) {
-    return res.status(400).json({ message: "Email and password are required" });
-  }
-  const { email, password } = req.body;
-
-  const user = await prisma.user.findUnique({
-    where: { email },
-  });
-
-  if (!user || !await bcrypt.compare(password, user.passwordHash)) {
-    return res.status(401).json({ message: "Invalid credentials" });
-  }
-
-  const token = jwt.sign({ email, id: user.id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
-  return res.json({ token });
-
-});
-
-router.post("/register", async (req, res) => {
-  try {
-    const { email, password, name } = req.body;
-
-    if (!email || !password) {
-      return res.status(400).json({ error: "email and password required" });
-    }
-
-    const passwordHash = await bcrypt.hash(password, 12);
-    const user = await prisma.user.create({
-      data: {
-        email,
-        name,
-        passwordHash,
-      },
-      select: {
-        id: true,
-        email: true,
-        name: true,
-        createdAt: true,
-      },
-    });
-
-    res.status(201).json(user);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "internal error" });
-  }
-});
-
-
-router.get("/me", authenticateToken, async (req, res) => {
-  const authReq = req as AuthenticatedRequest;
-  return res.json({ user: authReq.user });
-});
+router.post("/login", authController.login);
+router.post("/register", authController.register);
+router.get("/me", authenticateToken, authController.me);
 
 export default router;
