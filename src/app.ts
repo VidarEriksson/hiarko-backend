6cda9bcc69459f3affdfa32f2e1e38053ebd6959
vidar <vidar03@hotmail.com>
2026-01-05 23:18:03 +0100

    add dev cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import authRoutes from "./routes/auth";
 import boardRoutes from "./routes/board";
+import cors from "cors";
 
 export function createApp() {
   const app = express();
+
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+  );
+
   app.use(express.json());
 
   app.use("/auth", authRoutes);
