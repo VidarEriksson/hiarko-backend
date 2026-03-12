aa694b038a45e58c6920015d736c8d623adc304c
vidar <vidar03@hotmail.com>
2026-03-12 21:41:35 +0100

    wip

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 import crypto from "crypto";
 
 const JWT_SECRET = process.env.JWT_SECRET!;
-const JWT_EXPIRES_IN = "1";
+const JWT_EXPIRES_IN = "1h";
 const REFRESH_TOKEN_EXPIRES_DAYS = 7;
 
 if (!JWT_SECRET) {
